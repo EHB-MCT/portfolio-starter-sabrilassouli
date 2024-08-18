1915bdd31e7524f90d58890bfb5dcf0c12d4713a
sabrilassouli <sabri.lassouli@student.ehb.be>
2024-08-19 00:40:17 +0200

    created log in and sign up functions
    created a login and a sign up function with password hash

--- schoolForum/routes/index.js
+++ schoolForum/routes/index.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require('dotenv').config();
 
 
-const userRoutes = require('./user');
-const courseRoutes = require('./courses');
-const questionRoutes = require('./question');
-const answerRoutes = require('./answer');
+const userRoutes = require("./user");
+const courseRoutes = require("./courses");
+const questionRoutes = require("./question");
+const answerRoutes = require("./answer");
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 
-
-app.use('/users', userRoutes);
-app.use('/courses', courseRoutes);
-app.use('/questions', questionRoutes);
-app.use('/answers', answerRoutes);
+app.use("/users", userRoutes);
+app.use("/courses", courseRoutes);
+app.use("/questions", questionRoutes);
+app.use("/answers", answerRoutes);
 
 const port = process.env.PORT || 3000;
 
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
